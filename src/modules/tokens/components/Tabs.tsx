dfc4a65fb69774f2bb527f77f4b2944f698899c6
Ashutosh Lakshakar <ashutoshlakshakar@Ashutoshs-MacBook-Air.local>
2025-12-28 00:08:11 +0530

    feat:fix tab ui

--- src/modules/tokens/components/Tabs.tsx
+++ src/modules/tokens/components/Tabs.tsx
@@ -16,12 +16,12 @@ export default memo(function Tabs({ active, setActive }: Props) {
   ];
 
   return (
-    <div className="flex gap-3 mb-3">
+    <div className="flex gap-1 sm:gap-3 mb-3">
       {items.map(i => (
         <button
           key={i.key}
           onClick={() => setActive(i.key)}
-          className={`px-3 py-1 rounded-md text-sm transition
+          className={`px-3 py-1 rounded-md text-xs sm:text-sm transition
           ${active === i.key ? "bg-blue-600 text-white" : "bg-gray-800 text-gray-400"}`}
         >
           {i.label}
